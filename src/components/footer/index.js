3497f2e52dc9f5ab92a170e221f7bacc30d0e3ee
Alexander <57184332+dazn311@users.noreply.github.com>
2021-05-13 15:59:22 +0300

    ref footer - add build caption

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,8 +1,11 @@
 import React  from 'react';
 import Typography from '@material-ui/core/Typography';  
 import Link from '@material-ui/core/Link'; 
- 
+
+import {formatDateISO} from '../../hoc/formatDate'
 export default function Copyright() {
+    const newDate = new Date().toLocaleDateString() + ' (' + new Date().getHours() + ':' + new Date().getMinutes() + ')';
+
     return (
       <Typography variant="body2" color="textSecondary" align="center">
         {'Copyright © '}
@@ -10,7 +13,7 @@ export default function Copyright() {
           МосГеоТрест
         </Link>{' '}
         {new Date().getFullYear()}
-        {'.'}
+        {'.'}   <span style={{color:'grey'}} >build: {newDate}</span>
       </Typography>
     );
   }
